b0e279aa7eb601baaebc26df40ccfd84c63179a7
ChenxiiCheng <chenxic1011@gmail.com>
2019-10-23 14:15:00 -0400

    Prepared for deployment

---
+++ build/precache-manifest.31d53a6941b0a52208351dda5926b54c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15c6b509d80a579cb0bdcff8b320a24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2730b7c41138ce66cc18",
+    "url": "/static/css/main.d56a091c.chunk.css"
+  },
+  {
+    "revision": "246538ad78c967d385aa",
+    "url": "/static/js/2.bdb3cc22.chunk.js"
+  },
+  {
+    "revision": "2730b7c41138ce66cc18",
+    "url": "/static/js/main.6c5c47e3.chunk.js"
+  },
+  {
+    "revision": "8932f18fb6c7a20ec416",
+    "url": "/static/js/runtime-main.24bcab21.js"
+  },
+  {
+    "revision": "fc67ab97d9bd86799992b5c9f4e600bb",
+    "url": "/static/media/admin-react.fc67ab97.png"
+  },
+  {
+    "revision": "1eee4c51ca02bca0a2f04e4dd9ddce82",
+    "url": "/static/media/admin-vue.1eee4c51.png"
+  },
+  {
+    "revision": "ce2cf0b7468692f07b6ad49d871fc8a3",
+    "url": "/static/media/cooking-react.ce2cf0b7.jpeg"
+  },
+  {
+    "revision": "c11a2ee26983538e7c56f7d9d6d4afb0",
+    "url": "/static/media/mypic.c11a2ee2.png"
+  },
+  {
+    "revision": "d7cbae32f0debaa4f55fa3bed5b5d5b4",
+    "url": "/static/media/reddit-vue.d7cbae32.png"
+  }
+]);
